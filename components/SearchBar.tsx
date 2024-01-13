f158922c6d5440169b44c3443899d97ec6d79d9e
sahil-gusain <sahilgusainddn@gmail.com>
2024-01-13 16:22:12 +0530

    search bar created

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -22,12 +22,39 @@ const SearchBar = () => {
   const [manufacturer, setManuFacturer] = useState("");
   const [model, setModel] = useState("");
 
-
+  const router = useRouter();
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    if(manufacturer.trim()==="" && model.trim()==""){
+      return alert("Please provide some input");
+    }
+    updateSearchParams(model.toLowerCase(),manufacturer.toLowerCase());
+
   };
+
+  const updateSearchParams=(model : string,manufacturer:string)=>{
+
+       const searchParams = new URLSearchParams(window.location.search);
+
+       console.log(model,manufacturer);
+       if(model){
+        searchParams.set("model",model);
+       }else{
+        searchParams.delete("model")
+       }
+
+       
+       if(manufacturer){
+        searchParams.set("manufacturer",manufacturer);
+       }else{
+        searchParams.delete("manufacturer")
+       }
+       const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+       router.push(newPathname);
+  }
 
   return (
     <form className='searchbar' onSubmit={handleSearch}>
@@ -36,7 +63,27 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManuFacturer={setManuFacturer}
         />
+        <SearchButton otherClasses="sm:hidden"/>
        </div>
+       <div className='searchbar__item'>
+          <Image 
+            src='/model-icon.png'
+            alt="car model"
+            width={25}
+            height={25}
+            className='absolute w-[20px] h-[20px] ml-4'
+            />
+            <input
+            type='text'
+            name='model'
+            value={model}
+            onChange={(e) => setModel(e.target.value)}
+            placeholder='Tiguan...'
+            className='searchbar__input'
+          />
+        <SearchButton otherClasses='sm:hidden' />
+       </div>
+       <SearchButton otherClasses='max-sm:hidden' />
     </form>
   );
 };
